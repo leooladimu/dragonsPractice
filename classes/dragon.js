e12cf9b0fd5d9a3f0a09f252fed373630b406c44
Leo Ọládimú <82811737+leooladimu@users.noreply.github.com>
2022-12-13 17:06:51 -0500

    3

--- classes/dragon.js
+++ classes/dragon.js
@@ -3,16 +3,38 @@ class Dragon {
     this.name = name;
     this.color = color;
   }
-  // const newAccount = new BankAccount('Will Alexander', 500);
-
   breathesFire() {
-    return `${this.name} breathes fire everywhere! BURN!!!!`;
+    return `${this.name} breathes fire everywhere! BURN!!!!`
   }
-  static getDragons(...Dragon) {
-    return this.name;
+  static getDragons(...args) {
+    let dragonNames = []
+    args.forEach(arg => {
+      dragonNames.push(arg.name)
+    });
+    return dragonNames
   }
+  
 }
 
+
+const puff = new Dragon("Puff", "green");
+console.log(puff);
+console.log(puff.breathesFire());
+
+const john = new Dragon("John", "brown");
+console.log(john);
+console.log(john.breathesFire());
+
+const toothless = new Dragon("Toothless", "black");
+console.log(toothless);
+console.log(toothless.breathesFire());
+
+// Dragon { name: 'Toothless', color: 'black' }
+// Toothless breathes fire everywhere! BURN!!!!
+
+// const puff = new Dragon("Puff", "green");
+// const toothless = new Dragon("Toothless", "black");
+console.log(Dragon.getDragons(puff, toothless, john));
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
@@ -21,3 +43,18 @@ try {
 } catch {
   module.exports = null;
 }
+
+
+
+// constructor(name, color) {
+//   this.name = name;
+//   this.color = color;
+// }
+// // const newAccount = new BankAccount('Will Alexander', 500);
+// 
+// breathesFire() {
+//   return `${this.name} breathes fire everywhere! BURN!!!!`;
+// }
+// static getDragons(...Dragon) {
+//   return this.name;
+// }
